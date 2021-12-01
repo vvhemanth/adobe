4881166ce037ed25eb9d745cb36fdaff328ddb0c
hemanth <vvhemanth411@gmail.com>
2021-12-01 18:15:14 +0530

    code is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { JavascriptComponent } from './javascript/javascript.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JavascriptComponent
   ],
   imports: [
     BrowserModule,
